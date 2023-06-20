78ebb1f6e09e32cb9b980539aebfd3dd4a2fa2b4
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-06-20 11:41:58 -0400

    deleted schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,36 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_09_152240) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "courts", force: :cascade do |t|
-    t.string "park"
-    t.string "neighborhood"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "street"
-    t.string "notes"
-  end
-
-  create_table "games", force: :cascade do |t|
-    t.string "time"
-    t.boolean "bring_ball"
-    t.string "skill_level"
-    t.string "contact_info"
-    t.integer "user_id"
-    t.integer "court_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
