228f29bef6e05443832ab06cc82edac3d27301cf
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-06-23 14:02:00 -0400

    fetchUserfn AppCtrllr login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  before_action :authorize
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user 
+  end
+
+  private
+  
+  def render_unprocessable_entity_response(e)
+    render json: { errors: e.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: { error: "Not Found" }, status: :not_found
+  end
 end
--- client/src/context/user.js
+++ client/src/context/user.js
@@ -1,11 +1,16 @@
-import React, { useState, useEffect, useParams } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const UserContext = React.createContext();
 
 function UserProvider({ children }) {
-
+    const [user, setUser] = useState(false);
+    const [errors, setErrors] = useState([]);
     const [loginError, setLoginError] = useState([]);
+    // const [signUpError, setSignUpError] = useState([]);
+
+    const navigate = useNavigate()
+
     //have a clubs context and a match context?
     //what would go in these? any fns related to each
     //separate concerns
@@ -20,6 +25,59 @@ function UserProvider({ children }) {
     //match
     //editMatch
     //addNewMatch
+    useEffect(() => {
+        fetchUser('/me', 'GET')
+      }, []);
+
+      const fetchUser = async (url, method, body = false) => {
+        // setSignUpError([])
+        setLoginError([])
+    
+        const messages = (url, err) => {
+          if (url === "/signup"){
+          setSignUpError(err)
+        } else if (url === "/login") {
+          setLoginError(err)
+        }
+      } 
+    
+        try {
+          const options = {
+            method: method,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          };
+    
+          if (body) {
+            options.body = JSON.stringify(body);
+          }
+    
+          const response = await fetch(url, options);
+          const data = await response.json();
+    
+          if (response.ok) {
+            setUser(data);
+            navigate('/');
+            //changes to /matches/pending or whatever
+            //should matches be it's own page, and then on it are there buttons to see pending 
+            //matches and accepted matches and a prompt to create a new match?
+    
+          } else if (response.status === 401) {
+          
+            let err = data.errors.map((e, i) => <li key={i}>{e}</li>)
+            
+            messages(url, err)
+          }
+    
+      } catch (error) {
+     
+          let message = [<li>Server Unresponsive</li>]
+             
+            messages(url, message)
+        }
+      };
+
 
 
     const logout = () => {
@@ -29,16 +87,16 @@ function UserProvider({ children }) {
     return (
         <UserContext.Provider
           value={{
-            // user,
-            // setUser,
-            // fetchUser,
+            user,
+            setUser,
+            fetchUser,
             // signUpError,
-            // loginError,
-            // logout,
+            loginError,
+            logout,
             // editMatch,
-            // errors,
+            errors,
             // addMatch,
-            // courts,
+            // clubs,
             // setClubs
           }}
         >
