8a02e1cf3ae488b6a7e548c6b41662376805cb03
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-06-26 12:23:40 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Club.destroy_all
+Match.destroy_all
+
+puts "seeding clubs 🎾..."
+
+Club.create(club_name: 'Manhattan Tennis Club', street: '42nd St.', description: 'basement of Grand Central Station')
+Club.create(club_name: 'Brooklyn Tennis Club', street: 'Flatbush Ave.', description: 'near Prospect Park')
+Club.create(club_name: 'Queens Tennis Club', street: '12th Ave.', description: 'by the grocery store')
+Club.create(club_name: 'Bronx Tennis Club', street: '161st St.', description: 'next to Yankee Stadium')
+Club.create(club_name: 'Staten Island Tennis Club', street: 'Marine Ave.', description: 'near the fire station')
+
+
+# User.all.each do |user| 
+#     (1..3).each do |i| 
+#         court = Court.order('RANDOM()').first
+#         user.games.create!({
+#            time: Time.at(rand * Time.now.to_i).to_s,
+#            bring_ball: rand(2).zero?,
+#            skill_level: rand(1..10),
+#            contact_info: Faker::PhoneNumber.cell_phone,
+#            user_id: user.id,
+#            court_id: court.id
+#         })
+#     end
+# end
+
+
+puts "done seeding 🎾!"
