92cc237ce16fd07f0f70034f7edad13d818cfcaf
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-06-30 14:15:10 -0400

    added matches ctrllr

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,37 @@
 class MatchesController < ApplicationController
+
+    def create
+        game = @current_user.matches.create!(match_params)
+        render json: match, status: :created
+    end
+
+    def index
+        render json: @current_user.matches
+    end
+  
+    def show
+        match = @current_user.matches.find(params[:id])
+        render json: match
+    end
+    
+    def update
+        match = @current_user.matches.find(params[:id])
+        match.update!(match_params)
+        render json: match
+    end
+
+    def destroy
+        match = @current_user.matches.find(params[:id])
+        match.destroy
+        head :no_content
+    end
+
+
+    private
+
+    def match_params
+        params.permit(:club, :date, :time, :skill_level, :contact_info, :status)
+    end
+  
+
 end
