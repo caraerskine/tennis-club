02d0bf90e598257ba91f50c4c3441dbe9e80d6d6
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-07-02 19:51:06 -0400

    changed photos in seeds

--- app/serializers/club_serializer.rb
+++ app/serializers/club_serializer.rb
@@ -1,4 +1,4 @@
 class ClubSerializer < ActiveModel::Serializer
-  attributes :id, :club_name, :street, :description
+  attributes :id, :club_name, :street, :description, :club_img
   has_many :matches, serializer: ClubMatchSerializer
 end
