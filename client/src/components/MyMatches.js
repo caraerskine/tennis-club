06ef309ce4a204d7edb94f42d2c92d92f5429bd7
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-07-09 16:19:22 -0400

    mymatches consol

--- client/src/components/MyMatches.js
+++ client/src/components/MyMatches.js
@@ -12,18 +12,25 @@ function MyMatches() {
 //show pending matches, accepted matches and completed matches
 //do these then each need their own components? No.
 
+//just do user.matches
+//pending/accepted/completed dependent on sender and receiver
+//show each kind under each tag, in a card?
+
 //should it be on a card? like "MyMatchCard" ?
     <>
 
       <ol>
-
-
-
-
-
+        <br/>
+        <b>Pending Matches</b> <br />
+        <br />
+        <br />
+        <b>Accepted Matches</b> <br />
+        <br />
+        <br />
+        <b>Completed Matches</b> <br />
       </ol>
       
-      <div>
+      {/* <div>
               <br></br>
               <Link to={`/matches/pending`}>
                   <button>Pending Matches</button>
@@ -34,7 +41,7 @@ function MyMatches() {
               <Link to={`/matches/completed`}>
                   <button>Completed Matches</button>
               </Link>
-      </div>
+      </div> */}
     </>
 
   )
