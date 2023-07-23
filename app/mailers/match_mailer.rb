75a6d3d8801799d2041fb877cde9bbd4b22116f4
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-07-23 19:27:25 -0400

    chg mailer

--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -1,7 +1,9 @@
 class MatchMailer < ApplicationMailer
-    my_email = ENV['MY_EMAIL']
+    # my_email = ENV['MY_EMAIL']
     
-    default from: my_email
+    # default from: my_email
+
+    default from: ENV['MY_EMAIL']
 
     def new_match_notification(receiver_email, match)
       @match = match
