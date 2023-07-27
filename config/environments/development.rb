0432e352efffd4b907760c5dbe2f3d9594fe28c3
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-07-27 12:49:50 -0400

    trying to fix actionmailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
     port: 587,
     domain: 'gmail.com', # Replace with your domain name
     user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'],
+    password: ENV['APP_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true,
     open_timeout:         5,
