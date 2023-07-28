a98e582f2bbe16a01b939fa940b4b5903608c82e
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-07-28 10:33:51 -0400

    notes on mailer added

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,3 +2,11 @@ class ApplicationMailer < ActionMailer::Base
   default from: my_email = ENV['MY_EMAIL']
   layout 'mailer'
 end
+
+
+# class ApplicationMailer < ActionMailer::Base
+#   default from: -> { @current_user.email if @current_user.present? }
+#   layout 'mailer'
+# end
+
+#might change?
--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -7,3 +7,14 @@ class MatchMailer < ApplicationMailer
     end
 end
 
+# class MatchMailer < ApplicationMailer
+#   default from: -> { @current_user.email if @current_user.present? }
+
+#   def new_match_notification(receiver_email, match)
+#     @match = match
+#     mail(to: receiver_email, subject: 'New Tennis Match Notification')
+#   end
+# end
+
+#might change later? does work w me as default sender
+
